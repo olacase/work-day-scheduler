b45d72147a27f7f11fde3f87c980cdf35c943150
olacase@gmail.com <olacase@gmail.com>
2022-03-10 17:01:56 -0600

    complete with a few hide element glitch and local storage fix issues

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # work-day-scheduler
+[githun=b](https://github.com/olacase/work-day-scheduler)
+
+![image] ./
+
+## Table of contents
+
+## Introduction
+code to create and organize a daily work schedule from 9am to 5pm.
+
+## Installation
+![Directions for use] Click on the url  
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,24 @@
-// #1 
+// #1 create a function to save inputs in local storage
 $(document).ready(function() {
 
     $('.saveBtn').on('click', function () {
-        var value = $(this).siblings('.description').val();
+        var value = $(this).siblings('.tasks').val();
         var time = $(this).parent().attr('id');
 
 
         localStorage.setItem(time, value);
 
 
-  // #2
+  // #2 create a notification message advising on any new input to the days schedule and display for 3 seconds
     $('.notification').addClass('show');
 
     setTimeout(function () {
         $('.notification').removeClass('show');
     
-    }, 5000);
+    }, 3000);
     });
 
-    // #3
+    // #3 make a function that updates the time block with local time and link CSS displaying current time color-codes
     function hourUpdater () {
         var currentHour = moment().hours();
 
@@ -45,16 +45,16 @@ $(document).ready(function() {
 
     var interval = setInterval(hourUpdater, 15000);
     // load any saved data from localStorage
-    $('#hour-9 .description').val(localStorage.getItem('hour-9'));
-    $('#hour-10 .description').val(localStorage.getItem('hour-10'));
-    $('#hour-11 .description').val(localStorage.getItem('hour-11'));
-    $('#hour-12 .description').val(localStorage.getItem('hour-12'));
-    $('#hour-13 .description').val(localStorage.getItem('hour-13'));
-    $('#hour-14 .description').val(localStorage.getItem('hour-14'));
-    $('#hour-15 .description').val(localStorage.getItem('hour-15'));
-    $('#hour-16 .description').val(localStorage.getItem('hour-16'));
-    $('#hour-17 .description').val(localStorage.getItem('hour-17'));
-    $('#hour-18 .description').val(localStorage.getItem('hour-18'));
+    $('#hour-9 .tasks').val(localStorage.getItem('hour-9'));
+    $('#hour-10 .tasks').val(localStorage.getItem('hour-10'));
+    $('#hour-11 .tasks').val(localStorage.getItem('hour-11'));
+    $('#hour-12 .tasks').val(localStorage.getItem('hour-12'));
+    $('#hour-13 .tasks').val(localStorage.getItem('hour-13'));
+    $('#hour-14 .tasks').val(localStorage.getItem('hour-14'));
+    $('#hour-15 .tasks').val(localStorage.getItem('hour-15'));
+    $('#hour-16 .tasks').val(localStorage.getItem('hour-16'));
+    $('#hour-17 .tasks').val(localStorage.getItem('hour-17'));
+    $('#hour-18 .tasks').val(localStorage.getItem('hour-18'));
 
 
 
